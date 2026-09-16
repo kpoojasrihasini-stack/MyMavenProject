c8bae92c07d5d09d717642613a540d8da5f37ab1
kpoojasrihasini-stack <k.poojahasini@gmail.com>
2026-09-16 12:03:54 +0530

    Add files via upload

--- src/main/java/com/portfolioproject/model/Asset.java
+++ src/main/java/com/portfolioproject/model/Asset.java
@@ -1,37 +1,33 @@
 package com.portfolioproject.model;
 
-public class Asset {
-	private String assetid;
-	private String assetname;
-	private String price;
-	public Asset(String assetid, String assetname, String price) 
-	{
-		this.assetid=assetid;
-		this.assetname=assetname;
-		this.price=price;
-	}
-	public String getAssetid() {
-		return assetid;
-	}
-	public void setAssetid(String assetid) {
-		this.assetid = assetid;
-	}
-	public String getAssetname() {
-		return assetname;
-	}
-	public void setAssetname(String assetname) {
-		this.assetname = assetname;
-	}
-	public String getPrice() {
-		return price;
-	}
-	public void setPrice(String price) {
-		this.price = price;
-	}
-	public void display() {
-		System.out.println("Asset ID:" + assetid);
-		System.out.println("Asset Name:" + assetname);
-		System.out.println("Asset Price:" + price);
-		
-	}
+public abstract class Asset {
+
+    private String assetId;
+    private String assetName;
+    private double purchasePrice;
+
+    // Constructor
+    public Asset(String assetId, String assetName, double purchasePrice) {
+
+        this.assetId = assetId;
+        this.assetName = assetName;
+        this.purchasePrice = purchasePrice;
+    }
+
+    // Getters
+
+    public String getAssetId() {
+        return assetId;
+    }
+
+    public String getAssetName() {
+        return assetName;
+    }
+
+    public double getPurchasePrice() {
+        return purchasePrice;
+    }
+
+    // Abstract method
+    public abstract double calculateCurrentValue();
 }
